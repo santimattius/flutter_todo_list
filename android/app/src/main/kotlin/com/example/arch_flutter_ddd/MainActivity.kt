0c3f34e6d4b9f87c9fc70605e2a25a528bd90b9e
Santiago Mattiauda <santimattius@gmail.com>
2020-11-25 19:22:16 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/arch_flutter_ddd/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.arch_flutter_ddd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/domain/auth/value_objects.dart
@@ -0,0 +1,32 @@
+import 'package:arch_flutter_ddd/domain/core/failures.dart';
+import 'package:arch_flutter_ddd/domain/core/value_objects.dart';
+import 'package:arch_flutter_ddd/domain/core/value_validators.dart';
+import 'package:dartz/dartz.dart';
+
+class EmailAddress extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  factory EmailAddress(String input) {
+    assert(input != null);
+    return EmailAddress._(
+      validateEmailAddress(input),
+    );
+  }
+
+  const EmailAddress._(this.value);
+}
+
+class Password extends ValueObject<String> {
+  @override
+  final Either<ValueFailure<String>, String> value;
+
+  factory Password(String input) {
+    assert(input != null);
+    return Password._(
+      validatePassword(input),
+    );
+  }
+
+  const Password._(this.value);
+}
